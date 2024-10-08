1abd1af15c63cc01befd702fd56ce1cb937a8d9f
Hicham Alaoui <e.alaoui.hicham@gmail.com>
2024-10-08 17:52:20 +0200

    Initial commit for EkwateurBillingAPI

---
+++ src/main/java/com/ekwateur/EkwateurBillingAPI/EkwateurBillingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ekwateur.EkwateurBillingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkwateurBillingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkwateurBillingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ekwateur/EkwateurBillingAPI/EkwateurBillingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekwateur.EkwateurBillingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkwateurBillingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
