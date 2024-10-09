a30163fd6954967121f0fa6f8e6238fd0d899d3e
Hicham Alaoui <e.alaoui.hicham@gmail.com>
2024-10-09 19:31:47 +0200

    Implementation of billing controller

--- src/main/java/com/ekwateur/EkwateurBillingAPI/controller/BillingController.java
+++ src/main/java/com/ekwateur/EkwateurBillingAPI/controller/BillingController.java
@@ -1,9 +1,13 @@
 package com.ekwateur.EkwateurBillingAPI.controller;
 
+import com.ekwateur.EkwateurBillingAPI.dto.IndividualClientDTO;
+import com.ekwateur.EkwateurBillingAPI.dto.ProClientDTO;
 import com.ekwateur.EkwateurBillingAPI.service.BillingService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 /**
  * Controller for managing billing operations.
@@ -25,5 +29,18 @@ public class BillingController {
      */
     public BillingController(BillingService billingService) {
         this.billingService = billingService;
+    }
+
+    @PostMapping("/calculate/pro")
+    public ResponseEntity<BigDecimal> calculateProBill(@RequestBody ProClientDTO proClientDto, @RequestParam int kWhElectricity, @RequestParam int kWhGas) {
+        BigDecimal billAmount = billingService.calculateProBill(proClientDto, kWhElectricity, kWhGas);
+        return ResponseEntity.ok(billAmount);
+    }
+
+
+    @PostMapping("/calculate/individual")
+    public ResponseEntity<BigDecimal> calculateIndividualBill(@RequestBody IndividualClientDTO individualClientDto, @RequestParam int kWhElectricity, @RequestParam int kWhGas) {
+        BigDecimal billAmount = billingService.calculateIndividualBill(individualClientDto, kWhElectricity, kWhGas);
+        return ResponseEntity.ok(billAmount);
     }
 }
